f783ff1d29daf442c3c63cb8bfc24b30c0fdeeee
Jeong eunji <wjddmsl000@naver.com>
2019-11-25 14:03:16 +0900

    modified manage.css

---
+++ www/management/htmlp/support.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/popup.css">
+    <title>10Jo</title>
+</head>
+
+<body>
+    <header>
+        <h1>10Jo</h1>
+        <div id="login"></div>
+    </header>
+    <nav>
+        <ul>
+            <li>
+                <img src="../image/employee.png" width="50px" alt="employee_icon" />직원관리
+                <ul>
+                    <li><a href="list.php">직원목록</a></li>
+                    <li><a href="attenndance.php">근태관리</a></li>
+                    <li><a href="floor.php">플로어업무</a></li>
+                </ul>
+            </li>
+            <li >
+                <img src="../image/store.png" width="50px" alt="store_icon" /> <span>시설관리</span> 
+                <ul>
+                    <li><a href="order.php">주문발주</a></li>
+                    <li><a href="support.php">기술지원</a></li>
+                    <li><a href="clean.php">청결관리</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+
+</body>
+
+</html>
