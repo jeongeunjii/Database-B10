c3433c23a2d4678309521491a577978a17782d15
SungjuYun <bravokan7@gmail.com>
2019-11-30 11:28:42 +0900

    Saving

--- www/management/htmlp/list.php
+++ www/management/htmlp/list.php
@@ -6,118 +6,147 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="../css1/layout2.css">
-    <link rel="stylesheet" type="text/css" href="../css1/list.css">
+<meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/layout.css">
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../css/menu.css">
+    <link rel="stylesheet" type="text/css" href="../css/button.css">
+    <link rel="stylesheet" type="text/css" href="../css/list.css">
     <script src="../script/list.js" type="text/javascript"></script>
     <title>10Jo</title>
 </head>
 
 <body>
     <header>
-        <a href="home.php"><h1>10Jo</h1></a>
-        <div id="login"><p>
+        <a href="list.php"><h1>10Jo</h1></a>
+        
+        <div id="login">
             <?php
                 if (isset($_SESSION['ID'])){
-                    echo "( ".$_SESSION['ID']." / ".$_SESSION['PW']." / ".$_SESSION['DEP']." )";
+                    echo "<p>( ".$_SESSION['ID']." / ".$_SESSION['PW']." / ".$_SESSION['DEP']." )</p>";
             ?>
-            </p><a href="../php/logout.php">Logout</a>
+                    <a href="../php/logout.php"><button class="button">Logout</button></a>
             <?php
                 }
                 else {
-                    echo '<a href="login.php">login</a>';
+                    header("Location: ../htmlp/login.php");
                 }
             ?>
         </div>
     </header>
-    <section>
+    <input type="checkbox" id="menu_state" checked>
     <nav>
-        <ul>
-            <li>
-                <img src="../image/employee.png" width="50px" alt="employee_icon" /> <span>직원관리</span>
-                <ul>
-                    <?php  
-                    if ($_SESSION['DEP'] == "매니저") { 
-                    ?>
-                        <li><a href="list.php">직원목록</a></li>
-                    <?php
-                    }
-                    ?>
-                    <li><a href="attendance.php">근태관리</a></li>
-                    <?php 
-                    if ($_SESSION['DEP'] == "플로어") { ?>
-                        <li><a href="floor.php">플로어업무</a></li>
-                    <?php
-                    }
-                    ?>
-                    <?php 
-                    if ($_SESSION['DEP'] == "기술지원") { ?>
-                        <li><a href="repair.php">정비업무</a></li>
-                    <?php
-                    }
-                    ?>
-                </ul>
-            </li>
-            <li >
-                <img src="../image/store.png" width="50px" alt="store_icon" /> <span>시설관리</span> 
-                <ul>
-                    <li><a href="order.php">주문발주</a></li>
-                    <?php  
-                    if ($_SESSION['DEP'] == "매니저") { 
-                    ?>
-                        <li><a href="technical.php">시설정비</a></li>
-                        <li><a href="clean.php">청결관리</a></li>
-                    <?php
-                    }
-                    ?>
-                </ul>
-            </li>
-        </ul>
-    </nav>
-    <main>
-    <?php
-        if ( $_SESSION['DEP'] == "매니저"){
-    ?>
-            <div>
-                <button onclick="addlist();">추가</button>
-                <button onclick="editlist();">수정</button>
-                <button onclick="deletelist();">삭제</button>
-            </div>
-    <?php
-        }
-    ?>
-        
-    <?php
-        try {
-            if ( $_SESSION['DEP'] == "매니저"){
-                $db = new PDO("mysql:dbname=movie; host=13.125.252.255; port=3306", "root", "1234");
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $db->query("set session character_set_connection=utf8;");
-                $db->query("set session character_set_results=utf8;");
-                $db->query("set session character_set_client=utf8;");
-
-                $rows = $db->query("SELECT * FROM 직원관리 ORDER BY 부서");
-                foreach ($rows as $row) {
-                ?>
-                <li>
-                    <?= $row["사번"] ?>
-                    <?= $row["이름"] ?>
-                    <?= $row["부서"] ?>
-                    <?= $row["생년월일"] ?>
-                    <?= $row["전화번호"] ?>
+        <label for="menu_state"><img src="../image/menu.png" alt="menu_icon" /></label>
+        <div id="fir_category">
+            <ul> <div class="category_name">직원관리</div>
+                <li><img src="../image/employee.png" alt="employee_icon" />
+                    <a href="list.php">
+                        직원목록
+                    </a>
                 </li>
+                <li><img src="../image/attendance.png" alt="attendance_icon" />
+                    <a href="attendance.php">
+                        근태관리
+                    </a>
+                <!--(직원용)<li><a href="attendance_emp.html"onclick="window.open(this.href, 'popup', 'width=300,height=300,location=no,status=no');">플로어업무</a></li>-->
+                </li>
+                <li><img src="../image/floor.png" alt="floor_icon" />
+                    <a href="floor.php">
+                        플로어업무
+                    </a>
+                </li>
+                <!--(직원용)<li><a href="popup_floor_emp.html"onclick="window.open(this.href, 'popup', 'width=300,height=300,location=no,status=no');">플로어업무</a></li>-->
+                <li><img src="../image/engineer.png" alt="engineer_icon" />
+                    <a href="repair.php">
+                        기술업무
+                    </a>
+                </li>
+                <!--(직원용)<li><a href="popup_engineer_emp.html"onclick="window.open(this.href, 'popup', 'width=300,height=300,location=no,status=no');">기술업무</a></li>-->
+                <!--기술업무, 플로어업무 직원용 페이지는 새 페이지 띄우는 것보다 팝업으로 하는 게 더 깔끔할 것 같아서 팝업으로 처리 했습니다!-->
+            </ul>
+        </div>
+        <div id="sec_category">
+            <ul> <div class="category_name">시설관리</div>
+                <li><img src="../image/order.png" alt="order_icon" />
+                    <a href="order.php">
+                        주문발주
+                    </a>
+                </li>
+                <li><img src="../image/clean.png" alt="clean_icon" />
+                    <a href="clean.php">
+                        청결관리
+                    </a>
+                </li>
+                <li><img src="../image/staffing.png" alt="staffing_icon" />
+                    <a href="technical.php">
+                        기술지원
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+	<main>
+        <section>
+            <h2>직원목록</h2>
+            <div id="container">
+            
+            <!-- 테이블 입력칸에 input하고 검색버튼 누르면 그에 맞는 테이블만 출력 -->
+            <div class="btn_etc"><button class="button" onclick="addlist();">
+                추가</button></div>
+            <div class="btn_etc"><button class="button" onclick="deletelist();">
+                삭제</button></div>
+            <div class="btn_etc"><button class="button" onclick="editlist();">
+                수정</button></div>
+
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>사번</th>
+                        <th>이름</th>
+                        <th>부서</th>
+                        <th>생년월일</th>
+                        <th>전화번호</th>
+                    </tr>
+                </thead>
+                <tbody>
                 <?php
-                }
-            }
-        } catch (PDOException $ex) {
-    ?>
-        <p>Sorry, a database error occurred. Please try again later.</p>
-        <p>(Error details:
-            <?= $ex->getMessage() ?>)</p>
-    <?php
-        }
-    ?>
+                try {
+                    
+                    $db = new PDO("mysql:dbname=movie; host=52.78.148.203; port=3306", "root", "1234");
+                    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    $db->query("set session character_set_connection=utf8;");
+                    $db->query("set session character_set_results=utf8;");
+                    $db->query("set session character_set_client=utf8;");
+
+                    $rows = $db->query("SELECT * FROM 직원관리 ORDER BY 부서");
+                    foreach ($rows as $row) {
+                ?>
+                        <tr>
+                            <td><?=$row["사번"]?></td>
+                            <td><?= $row["이름"] ?></td>
+                            <td><?= $row["부서"] ?></td>
+                            <td><?= $row["생년월일"] ?></td>
+                            <td><?= $row["전화번호"] ?></td>
+                        </tr>
+                <?php
+                    }
+                    
+                } catch (PDOException $ex) {
+                ?>
+                    <p>Sorry, a database error occurred. Please try again later.</p>
+                    <p>(Error details: <?= $ex->getMessage() ?>)</p>
+                <?php
+                    }
+                ?>		
+                </tbody>
+            </table>
+        </div>
+        </section>
     </main>
+ 
+
     <div id="add">
         <form method="post" action="../php/addlist.php">
             <span>이름 : </span><input type="text" name="name" placeholder="이름"/><br>
@@ -146,7 +175,6 @@
         </form>
     </div>
 
-    </section>
 </body>
 
 </html>
--- www/management/htmlp/login.php
+++ www/management/htmlp/login.php
@@ -9,68 +9,84 @@ http-equiv="X-UA-Compatible" content="ie=edge"> <title>Document</title> </head>
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="../css1/layout2.css">
-        <title>10Jo</title>
-    </head>
+<head>
+    <title>10Jo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">	
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
 
-    <body>
-        <header>
-            <a href="home.php"><h1>10Jo</h1></a>
-        </header>
-        <section>
-        <nav>
-        <ul>
-            <li>
-                <img src="../image/employee.png" width="50px" alt="employee_icon" /> <span>직원관리</span>
-                <ul>
-                    <?php  
-                    if ($_SESSION['DEP'] == "매니저") { 
-                    ?>
-                        <li><a href="list.php">직원목록</a></li>
-                    <?php
-                    }
-                    ?>
-                    <li><a href="attendance.php">근태관리</a></li>
-                    <?php 
-                    if ($_SESSION['DEP'] == "플로어") { ?>
-                        <li><a href="floor.php">플로어업무</a></li>
-                    <?php
-                    }
-                    ?>
-                    <?php 
-                    if ($_SESSION['DEP'] == "기술지원") { ?>
-                        <li><a href="repair.php">정비업무</a></li>
-                    <?php
-                    }
-                    ?>
-                </ul>
-            </li>
-            <li >
-                <img src="../image/store.png" width="50px" alt="store_icon" /> <span>시설관리</span> 
-                <ul>
-                    <li><a href="order.php">주문발주</a></li>
-                    <?php  
-                    if ($_SESSION['DEP'] == "매니저") { 
-                    ?>
-                        <li><a href="technical.php">시설정비</a></li>
-                        <li><a href="clean.php">청결관리</a></li>
-                    <?php
-                    }
-                    ?>
-                </ul>
-            </li>
-        </ul>
-        </nav>
-        <main>
+<body>
+    <div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form" method="post" action="../php/login.php">
+					<span class="login100-form-title p-b-26">
+						10Jo Management
+					</span>
+
+					<div class="wrap-input100 validate-input">
+						<input class="input100" type="text" name="password">
+						<span class="focus-input100" data-placeholder="이름"></span>
+					</div>
+
+
+					<div class="wrap-input100 validate-input">
+						<span class="btn-show-pass">
+							<i class="zmdi zmdi-eye"></i>
+						</span>
+						<input class="input100" type="password" name="id">
+						<span class="focus-input100" data-placeholder="사번"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn" >
+								Login
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+	<script src="vendor/select2/select2.min.js"></script>
+
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+
+	<script src="js/main.js"></script>
+
+        <!-- <main>
             <form method="post" action="../php/login.php">
                 <input type="text" name="id" placeholder="사번"/>
                 <input type="text" name="password" placeholder="이름"/>
                 <input type="submit" value="login"/>
             </form>
-        </main>
-        </section>
-    </body>
-
+        </main> -->
+</body>
 </html>
