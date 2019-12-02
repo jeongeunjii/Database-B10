6ab726321a4e5954b679266157aafdb2666d601d
Unknown <pyl9983@naver.com>
2019-12-02 18:48:51 +0900

    change post

---
+++ www/TicketingPHP/common/db.php
@@ -0,0 +1,32 @@
+<?php
+
+$dbHost = "52.78.148.203";
+$dbName = "movie";
+$dbUser = "root";
+$dbPass = "1234";
+
+$db = new PDO("mysql:host={$dbHost};dbname={$dbName}; port=3306", $dbUser, $dbPass);
+$db->exec("set names utf8");
+
+session_start();
+
+$today = date("Y-m-d");
+
+function isEmpty($str) {
+  if ($str == null || $str == "") { return true; }
+  else { return false; }
+}
+
+function conlog($data){
+  echo "<script>console.log('" . $data . "');</script>";
+}
+
+function replace($url){
+  echo "<script>location.replace('" .$url. "')</script>";
+}
+
+function arl($msg){
+  echo "<script>alert('" .$msg. "');</script>";
+}
+
+?>
---
+++ www/TicketingPHP/login/join.php
@@ -0,0 +1,20 @@
+<?php
+  include "../common/db.php";
+
+  $id = $_POST["id"];
+  $ps = $_POST["ps"];
+  $laName = $_POST["laName"];
+  $fiName = $_POST["fiName"];
+  $birth = (string)$_POST["year"] . "-" . (string)$_POST["mon"] . "-" . (string)$_POST["day"];
+  $hp = $_POST["hp"];
+
+  $test_ps = $db->query("select 아이디 from 회원 where 아이디 ='$id'");
+  if ($test_ps->rowCount() > 0) {
+    //arl("오류 : 중복된 아이디 입니다.");
+    replace('join.html');
+  }
+  else if (!isEmpty($id) && !isEmpty($ps) && !isEmpty($laName) && !isEmpty($fiName) && !isEmpty($birth) && !isEmpty(hp)) {
+    $join = $db->query("insert into 회원 (아이디,비밀번호,이름_성,이름_이름,생일,전화번호) values('$id','$ps','$laName','$fiName','$birth','$hp')");
+    replace('../index.html');
+  }
+ ?>
